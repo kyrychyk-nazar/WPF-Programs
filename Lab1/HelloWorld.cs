66347ad40f16097af4c89dc108e944511e232ef7
kyrychyk-nazar <nazarkiricik43@gmail.com>
2026-03-18 02:37:16 +0200

    First commit

---
+++ Lab1/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public struct HelloWorld
+    {
+        public readonly string str;
+        public HelloWorld()
+        {
+            str = "The button was pressed";
+        }
+    }
+}
