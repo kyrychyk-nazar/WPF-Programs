f76f08b840cf317f25ac93ee0f66e9f79ae40bfa
kyrychyk-nazar <nazarkiricik43@gmail.com>
2026-03-18 03:02:27 +0200

    Done 2-nd Lab

---
+++ Lab2/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string? filepath;
+        private WorkSpace? workSpace;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNewClick(object sender, RoutedEventArgs e)
+        {
+            LoadContent("");
+        }
+
+        private void OnOpenClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filepath = openFileDialog.FileName; // file path
+                string fileContent = File.ReadAllText(filepath);
+                LoadContent(fileContent);
+            }
+        }
+
+        private void OnSaveClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(filepath))
+            {
+                SaveFileDialog dialog = new SaveFileDialog
+                {
+                    Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                    DefaultExt = "txt"
+                };
+
+                bool? result = dialog.ShowDialog();
+
+                if (result != true)
+                    return;
+
+                filepath = dialog.FileName;
+            }
+
+            File.WriteAllText(filepath, workSpace?.TextField.Text);
+        }
+
+        private void OnCutClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(workSpace?.TextField.SelectedText))
+            {
+                workSpace.TextField.Cut();
+            }
+        }
+
+        private void OnCopyClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(workSpace?.TextField.SelectedText))
+            {
+                workSpace.TextField.Copy();
+            }
+        }
+
+        private void OnPasteClick(object sender, RoutedEventArgs e)
+        {
+            workSpace?.TextField.Paste();
+        }
+
+        private void LoadContent(string text)
+        {
+            workSpace = new WorkSpace(text);
+            SaveItem.IsEnabled = true;
+            WorkSpaceContent.Content = workSpace;
+        }
+    }
+}
---
+++ Lab2/WorkSpace.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for WorkSpace.xaml
+    /// </summary>
+    public partial class WorkSpace : UserControl
+    {
+        private string text { get; set; }
+        public WorkSpace(string text)
+        {
+            InitializeComponent();
+            this.text = text;
+            LoadText();
+        }
+
+        public void LoadText()
+        {
+            TextField.Text = text ?? "";
+        }
+
+        private void FontSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextField.FontSize = e.NewValue;
+        }
+    }
+}
